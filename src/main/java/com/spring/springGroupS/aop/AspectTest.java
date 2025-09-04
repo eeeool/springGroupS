d3feec614f54a6f160b76002f6c1cff678408846
eeeool <engo101010@gmail.com>
2025-09-04 10:50:27 +0900

    20250904_1050 aop/xml

--- src/main/java/com/spring/springGroupS/aop/AspectTest.java
+++ src/main/java/com/spring/springGroupS/aop/AspectTest.java
@@ -38,9 +38,8 @@ public class AspectTest {
 		System.out.println("   핵심코드에서 반환된 값: " + result.toString() + "\n");
 	}
 	
-	// 문제4: getAopServiceTest2() 메소드, getAopServiceTest3() 메소드, 모든 메소드가 수행하는데 걸리는 시간?
+	//문제4 : getAopServiceTest2()메소드와 getAopServiceTest3()메소드가 각각 수행하는데 걸리는 시간?
 	long startTime, endTime;
-	
 	@Pointcut("execution(* com.spring.springGroupS.service.Study1Service.getAopServiceTest5*(..))")
 	private void cut() {}
 	
@@ -48,15 +47,34 @@ public class AspectTest {
 	public void arroundAdvice2(ProceedingJoinPoint joinPoint) throws Throwable {
 		startTime = System.nanoTime();
 		
-		System.out.println("arroundAdvice2() 메소드 입니다. (전): " + joinPoint);
+		System.out.println("arroundAdvice2()메소드입니다. (전) : " + joinPoint);
 		joinPoint.proceed();
-		System.out.println("arroundAdvice2() 메소드 입니다. (후): " + joinPoint);
+		System.out.println("arroundAdvice2()메소드입니다. (후) : " + joinPoint);
 		
 		endTime = System.nanoTime();
 		
 		long res = endTime - startTime;
-		System.out.println("수행시간: " + res);
+		System.out.println("수행시간 : " + res);
 	}
+
 	
 	// 문제5: Study1service 객체안의 모든 메소드의 수행시간을 해당 메소드 이름과 함께 출력
+	int cnt = 0;
+	@Around("execution(* com.spring.springGroupS.service.Study1Service.*(..))")
+	//@Around("execution(* com.spring.springGroupS.service..*.*(..))")
+	public Object aroundAdvice5(ProceedingJoinPoint joinPoint) throws Throwable {
+		cnt++;
+		System.out.println(cnt + "번째 : 이곳은 aroundAdvice5메소드 입니다.");
+		long startTime = System.nanoTime();
+		try {
+		  Object result = joinPoint.proceed();
+		  return result;
+		} finally {
+			long endTime = System.nanoTime();
+			long executionTime = endTime - startTime;
+			System.out.println(cnt + "번 메소드 수행시간 : " + executionTime + "ns\n");
+			System.out.println(cnt + "번끝 : ******************************************************\n");
+		}
+	}
 }
+		
